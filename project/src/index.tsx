71896066666e97f080ce0ac2df560890eaf89245
talkingmachine <aeyueoq@gmail.com>
2023-02-18 22:46:17 +0300

    auth add part 1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import firebase from "firebase/app";
+import {initializeApp} from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth';
+import {auth}'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAKa0nRVXVCh25RsUN4sa-WDfMwYFzsaiE",
-  authDomain: "rt-chat-dc531.firebaseapp.com",
-  projectId: "rt-chat-dc531",
-  storageBucket: "rt-chat-dc531.appspot.com",
-  messagingSenderId: "1064480803286",
-  appId: "1:1064480803286:web:bb3135a35ab0cd5ef6fc77"
-};
+initializeApp({
+  apiKey: 'AIzaSyAKa0nRVXVCh25RsUN4sa-WDfMwYFzsaiE',
+  authDomain: 'rt-chat-dc531.firebaseapp.com',
+  projectId: 'rt-chat-dc531',
+  storageBucket: 'rt-chat-dc531.appspot.com',
+  messagingSenderId: '1064480803286',
+  appId: '1:1064480803286:web:bb3135a35ab0cd5ef6fc77'
+});
 
-//const app = initializeApp(firebaseConfig);
+git status
+const Context = createContext(null);
+const auth = firebase.auth();
+console.log(auth);
+const firestore = firebase.firestore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Context.Provider value={{auth, firebase, firestore}}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context.Provider>,
 );
