6b865aa150a1283e6318d0e6620fec012ee0d98b
talkingmachine <aeyueoq@gmail.com>
2023-02-19 23:46:57 +0300

    auth part 2

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,7 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {initializeApp} from 'firebase/app';
-import 'firebase/firestore';
-import {auth}'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 initializeApp({
   apiKey: 'AIzaSyAKa0nRVXVCh25RsUN4sa-WDfMwYFzsaiE',
@@ -14,11 +13,24 @@ initializeApp({
   appId: '1:1064480803286:web:bb3135a35ab0cd5ef6fc77'
 });
 
-git status
 const Context = createContext(null);
-const auth = firebase.auth();
-console.log(auth);
-const firestore = firebase.firestore();
+
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);// This gives you a Google Access Token. You can use it to access the Google API.
+    const token = credential.accessToken;
+    const user = result.user;// The signed-in user info.
+    // IdP data available using getAdditionalUserInfo(result)
+  }).catch((error) => {
+    const errorCode = error.code;// Handle Errors here.
+    const errorMessage = error.message;
+    const email = error.customData.email;// The email of the user's account used.
+    const credential = GoogleAuthProvider.credentialFromError(error);// The AuthCredential type that was used.
+    // ...
+  });
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
